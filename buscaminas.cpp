f8ac82869606ba1433e04a039f6f494a3ec6cc80
Elinostro <sebitasalguero@gmail.com>
2022-10-26 14:34:58 -0500

    Minas asignadas a lo largo del tablero

--- buscaminas.cpp
+++ buscaminas.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iterator>
 #include <map>
+#include <time.h>
+#include <cstdlib>
 using namespace std;
   
 int main(int argc, char** argv)
@@ -13,30 +15,33 @@ int main(int argc, char** argv)
         return 1;
     }
     cout <<"Tablero de "<<argv[1]<<"x"<<argv[1]<<" dificultad "<<argv[2][0]<<endl;
-    
     //Instanciación de variables
     bool bool_juego;
     char char_accion;
     float float_dificultad;
+    int int_area_tablero;
+    int int_aleatorio;
+    int int_cant_descubiertas;
+    int int_cant_marcadas;
     int int_cant_minas;
     int int_cont_minas;
     int int_coor_x;
     int int_coor_y;
     int int_dificultad;
-    int int_tablero;
+    int int_ite_filas;    
+    int int_lado_tablero;
     int int_resultado;
-    map<int, map<int,string>> map_juego;
-    map<int, map<int,string>>::iterator itr_map_juego;
-    map<int, string>::iterator ptr_map_juego;
-    
+    map<int, map<int,char>> map_juego;
+    map<int, map<int,char>>::iterator itr_map_juego;
+    map<int, char>::iterator ptr_map_juego;
     //Instanciación de tablero
-    int_tablero = int(argv[1][0]-48);
-
+    int_lado_tablero = int(argv[1][0]-48);
+    int_area_tablero = int_lado_tablero*int_lado_tablero;
     //Instanciación de cantidad de minas según dificultad
     int_dificultad = int(argv[2][0]-48);
     if(int_dificultad==1)
     {
-        float_dificultad = 0.2; //20% del tablero con minas
+        float_dificultad = 0.3; //30% del tablero con minas
     }
     else if(int_dificultad==2)
     {
@@ -44,10 +49,167 @@ int main(int argc, char** argv)
     }
     else
     {
-        float_dificultad = 0.9; //90% del tablero con minas
+        float_dificultad = 0.7; //70% del tablero con minas
     }
-    int_cant_minas = int((int_tablero*int_tablero)*float_dificultad);
+    int_cant_minas = int((int_area_tablero)*float_dificultad);
+    int_cant_marcadas = 0;
+    //Inflar tablero y asignar minas
+    int_cont_minas = int_cant_minas;
+    while(int_cont_minas!=0)//Solo saldrá de aquí si todas las minas son puestas
+    {
+        srand(time(0));
+        for(int i=0;i!=(int_area_tablero);i++) //Pasa por cada una de las celdas del tablero
+        {
+            map_juego[i][1]='-';
+            int_aleatorio = rand()%2;
+            if(map_juego[i][0]!=9 && int_aleatorio==1 && int_cont_minas>0)//Si el arreglo no tiene una mina y el aleatorio es 0 y las minas aún no son 0
+            {
+                map_juego[i][0]=9;
+                int_cont_minas--;
+            }
+        }
+    }
+    //Calcular campo de minas
+    for(int i=0;i!=int_area_tablero;i++)
+    {
+        //Todas las de la derecha, no entrar si col = int_lado_tablero
+        if(map_juego[i][0]!=9 && int_ite_filas!=int_lado_tablero)
+        {
+            if(map_juego[i+1][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        //Todas las de la izquierda, no entrar si col=0
+        if(map_juego[i][0]!=9 && int_ite_filas!=0)
+        {
+            if(map_juego[i-1][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        //Todas las de arriba
+        if(map_juego[i][0]!=9)
+        {
+            if(map_juego[i-int_lado_tablero][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        //Todas las de abajo
+        if(map_juego[i][0]!=9)
+        {
+            if(map_juego[i+int_lado_tablero][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        //Todas las de arriba derecha, no entrar si col=int_lado_tablero
+        if(map_juego[i][0]!=9 && int_ite_filas!=int_lado_tablero)
+        {
+            if(map_juego[i-int_lado_tablero+1][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        //Todas las de arriba izquierda, no entrar si col=0
+        if(map_juego[i][0]!=9 && int_ite_filas!=0)
+        {
+            if(map_juego[i-int_lado_tablero-1][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        //Todas las de abajo derecha, no entrar si col=int_lado_tablero
+        if(map_juego[i][0]!=9 && int_ite_filas!=int_lado_tablero)
+        {
+            if(map_juego[i+int_lado_tablero-1][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        //Todas las de abajo izquierda, no entrar si col=0
+        if(map_juego[i][0]!=9 && int_ite_filas!=0)
+        {
+            if(map_juego[i-int_lado_tablero][0]==9)
+            {
+                map_juego[i][0]++;
+            }
+        }
+        int_ite_filas++;
+        if(int_ite_filas==int_lado_tablero)
+        {
+            int_ite_filas=0;
+        }
+        
+    }
+    //Pintar tablero Claro
+    for(int i=0;i!=int_area_tablero;i++)
+    {
+        cout << int(map_juego[i][0]) << " ";
+        int_ite_filas++;
+        if(int_ite_filas==int_lado_tablero)
+        {
+            cout << endl;
+            int_ite_filas=0;
+        }
+    }
+    return 1;
+    
+    int_aleatorio = rand()%2;
+    map_juego[0][0]=int_aleatorio;
+    map_juego[0][1]='-';
+    int_aleatorio = rand()%2;
+    map_juego[1][0]=int_aleatorio;
+    map_juego[1][1]='-';
+    int_aleatorio = rand()%2;
+    map_juego[2][0]=int_aleatorio;
+    map_juego[2][1]='-';
+    int_aleatorio = rand()%2;
+    map_juego[3][0]=int_aleatorio;
+    map_juego[3][1]='-';
+    while(!((int_cant_descubiertas+int_cant_marcadas)==int_lado_tablero))
+    {
+        for(int i=0;i<int_lado_tablero; i++)
+        {
+            for(int j=0;j<int_lado_tablero;j++)
+            {
+                cout << map_juego[i][1];
+            }
+            cout << endl;
+        }
+        cin >> int_coor_x >> int_coor_y >> char_accion;
+        if(char_accion=='d' || char_accion=='D')
+        {
+            if (map_juego[0][0]!=1)//1 es igual a bomba 9
+            {
+                map_juego[0][1]==map_juego[0][0];
+                cout << "OK" << endl;
+                int_cant_descubiertas++;
+            }
+            else
+            {
+                cout << "BOOM!" << endl;
+                return 1;
+            }
+        }
+        else if(char_accion=='m' || char_accion=='M')
+        {
 
+        }
+    }
+    /* 
+    map_juego[0][0]=3;
+    map_juego[0][1]='M';
+    map_juego[1][0]=1;
+    map_juego[1][1]='-';
+    map_juego[2][0]=4;
+    map_juego[2][1]=map_juego[2][0];
+    cout << int_aleatorio << endl;
+    cout << int(map_juego[0][0]) <<" "<< map_juego[0][1] << endl;
+    cout << int(map_juego[1][0]) <<" "<< map_juego[1][1] << endl;
+    cout << int(map_juego[2][0]) <<" "<< int(map_juego[2][0]) << endl;
+     */
 }
 
 /*
@@ -74,9 +236,9 @@ Definir variables -- OK
     Para la dificultad del juego
         int_dificultad
     Para dimensión del tablero
-        int_tablero
+        int_lado_tablero
     Para el área del tablero
-        int_tablero*int_tablero
+        int_lado_tablero*int_lado_tablero
     Para las coordenadas
         int_coor_x
         int_coor_y
@@ -88,14 +250,14 @@ Definir variables -- OK
 Ingresar dimensión del tablero -- OK
     Ingrese la dimensión EJ: 5 será un tablero de 5x5
 
-Ingresar dificultad del tablero
+Ingresar dificultad del tablero -- OK
     Ingrese dificultad 1-3 siendo 3 nivel más difícil
         int_dificultad = 1 → area_tablero*0,1 = int_cant_minas
         int_dificultad = 2 → area_tablero*0,5 = int_cant_minas
         int_dificultad = 3 → area_tablero*0,9 = int_cant_minas
         Redondear al siguiente en todos los casos
 
-Asignar minas a posiciones
+Asignar minas a posiciones -- OK
     int_tmp_cant_minas=int_cant_minas
     mientras(int_temp_cant_minas > 0)
         entonces 
@@ -106,28 +268,34 @@ Asignar minas a posiciones
 
 Sumar minas vecinas y llenar map_juego ¿ITR termina siendo la dimensión del campo? parece que todo el campo
     ej:
-    0 1 9 
-    0 1 1
+    0 1 1 1
+    0 1 9 1
+    0 1 1 1
+    0 0 0 0
+
     0 0 0
+    1 2 2
+    9 9 2
     respecto a la posición central [5] del campo (celda que contiene un 1), map_juego[4].ptr->first[1] ptr-> [·]
     tiene una mina (9) en la posición superior derecha
     int_cont_mina=0 hasta cuando valida (superior derecha) int_cont_mina++
-    si (-(int_tablero)+1) == 9; entonces int_cont_mina++;
+    si (-(int_lado_tablero)+1) == 9; entonces int_cont_mina++;
     Así con todas las relaciones:
         relación de una posición respecto su alrededor
-            -(int_tablero)   (superior)
-            -(int_tablero)+1 (superior derecha)
+            -(int_lado_tablero)   (superior)
+            -(int_lado_tablero)+1 (superior derecha)
             +1                     (derecha)
-            +(int_tablero)+1 (inferior derecha)
-            +(int_tablero)   (inferior)
-            +(int_tablero)-1 (inferior izquierda)
+            +(int_lado_tablero)+1 (inferior derecha)
+            +(int_lado_tablero)   (inferior)
+            +(int_lado_tablero)-1 (inferior izquierda)
             -1                     (izquierda)
-            -(int_tablero)-1 (superior izquierda)
+            -(int_lado_tablero)-1 (superior izquierda)
         restricciones:
             si comprobación celda != error;
     luego asigna el valor int_cont_mina a map_juego[4].ptr->first[1] mediante
          map_juego[itr].insert(make_pair(int_cont_mina, "·"));
 
+    
 Pintar campo oscuro
     funcPintarCampoOscuro(){
         for(hasta que se acaben las celdas del map) ¿anidado?
@@ -202,4 +370,19 @@ BLOC DE NOTAS
 
         Para: descubiertas+marca=celdas
         ganó
+
+    Flujo de juego
+    selecciona una celda
+    la marca o destapa
+    si es bomba y la destapó
+        boom
+    si es bomba y la marcó
+        ok
+    si no es bomba y la destapó
+        ok
+        Ganó
+    vuelve a selecciona
+
+    Mientras descubiertas+marca=celdas sea falso
+
 */
